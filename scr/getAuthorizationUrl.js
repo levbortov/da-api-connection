5fad8d5acdb0379d952749b5aea5b577cd20d5e0
л→в <belobrov.lev@gmail.com>
2024-09-18 00:00:52 +0200

    Добваить функцию для получения ссылки на авторизацию

---
+++ scr/getAuthorizationUrl.js
@@ -0,0 +1,15 @@
+function getAuthorizationUrl(clientId, redirectUri, scope) {
+    const baseUrl = 'https://www.donationalerts.com/oauth/authorize'
+    const responseType = 'code'
+
+    // Создание URL с параметрами
+    const url = new URL(baseUrl);
+    url.searchParams.append('client_id', clientId)
+    url.searchParams.append('redirect_uri', redirectUri)
+    url.searchParams.append('response_type', responseType)
+    url.searchParams.append('scope', scope)
+
+    return url.toString()
+}
+
+module.exports = getAuthorizationUrl;
--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const express = require('express')
 const dotenv = require('dotenv')
 
+const getAuthorizationUrl = require('./scr/getAuthorizationUrl')
+
 dotenv.config()
 const app = express()
 const port = 3000
 
+const clientId = process.env.CLIENT_ID;
+const redirectUri = process.env.REDIRECT_URI;
+const scope = 'oauth-user-show oauth-goal-subscribe';
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Donation Alerts API connection')
+})
+
+app.get('/auth', (req, res) => {
+    const authorizationUrl = getAuthorizationUrl(clientId, redirectUri, scope)
+    res.redirect(authorizationUrl)
 })
 
 app.listen(port, () => {
